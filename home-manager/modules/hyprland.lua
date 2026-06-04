8e7e3cb01db7801e4ff879d47e5abcc53364abc7
Chelsea E. Manning <me@xychelsea.is>
2026-06-04 14:45:30 -0400

    fix monitor positions

--- home-manager/modules/hyprland.lua
+++ home-manager/modules/hyprland.lua
@@ -25,17 +25,18 @@ env("XDG_SESSION_TYPE", "wayland")
 env("XDG_SESSION_DESKTOP", "Hyprland")
 
 hl.monitor({
-  output = "DP-1",
-  mode = "2560x1440@144.01",
-  position = "0x0",
-  scale = 1,
+  output = "eDP-1",
+  mode = "2880x1920@120",
+  position = "0x1280",
+  scale = 1.5,
 })
 
 hl.monitor({
   output = "DP-2",
-  mode = "2560x1440@144.01",
-  position = "2560x0",
+  mode = "2560x1440@165.08Hz",
+  position = "1920x0",
   scale = 1,
+  transform = 1,
 })
 
 hl.config({
